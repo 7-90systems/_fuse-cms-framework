b481cf3abafc6e49272b9ae7627f20235801ffda
Michael Beaumont <michael@mariart.com.au>
2025-05-13 15:02:44 +1000

    Added the start of our FAQ post type.

---
+++ library/PostType/FAQ.php
@@ -0,0 +1,52 @@
+<?php
+    /**
+     *  This is our FAQs post type.
+     */
+    
+    namespace Fuse\PostType;
+    
+    use Fuse\PostType;
+    
+    
+    class FAQ extends PostType {
+        
+        /**
+         *  Object constructor.
+         */
+        public function __construct () {
+            parent::__construct ('fuse_faq', 'Question', 'FAQs', array (
+                'public' => false,
+                'publicly_queryable' => false,
+                'show_in_rest' => true,
+                'show_in_menu' => true,
+                'show_ui' => true,
+                'menu_icon' => 'dashicons-editor-help',
+                'supports' => array (
+                    'title',
+                    'editor',
+                    'page-attributes'
+                ),
+                'text_domain' => 'fuse'
+            ));
+        } // __construct ()
+        
+        
+        
+        
+        /**
+         *  Register our taxonomies.
+         */
+        public function registerTaxonomies () {
+            register_taxonomy ('fuse_faq_section', $this->getSlug (), array (
+                'labels' => array (
+                    'name' => __ ('FAQ Sections', 'fuse'),
+                    'singular_name' => __('Section', 'fuse')
+                ),
+                'public' => false,
+                'show_ui' => true,
+                'show_in_rest' => true,
+                'hierarchical' => true
+            ));
+        } // registerTaxonomies ()
+        
+    } // class FAQ
